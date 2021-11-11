0159f48f7cd6ecb850f7dd33a82d9c374a8c1a56
PhamJin <phamjin99@gmail.com>
2021-11-11 09:28:25 +0700

    api v1

---
+++ Plant-Management/api/register.api.js
@@ -0,0 +1,12 @@
+// function test(){
+//     // 
+//     if (!('fetch' in window)) {
+//         console.log('Fetch API not found, try including the polyfill');
+//         return;
+//       }
+//       else{
+//         alert("Helllo");
+//       }
+// }
+
+
